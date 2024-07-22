5a5b4300b552b9e82c9fa73d1cafe8aa25031ad6
PatDS28 <johnpatrickdesalit@gmail.com>
2024-07-22 22:51:54 +0800

    add book button added, book function constructor added, added book examples to the array

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const myLibrary = [];
 
-function Book() {
+function Book(author, title, pageNumber, haveRead) {
   // the constructor...
+  this.author = author;
+  this.title = title;
+  this.pageNumber = pageNumber;
+  this.haveRead = haveRead;
 }
+book1 = Book("Robert Greene", "40 Laws of Power", 452, "no")
+book2 = Book("Brianna West", "The Mountain is You", 241, "no")
 
-function addBookToLibrary() {
+myLibrary.push(book1);
+myLibrary.push(book2);
+
+function addBookToLibrary(author, title, pageNumber, haveRead) {
   // do stuff here
+  book = new Book(author, title, pageNumber, haveRead);
+  myLibrary.push(book);
 }
