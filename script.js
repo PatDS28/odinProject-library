37ddbd4e3baaf4c8ac81c40a5e7e01ae2f5a63e7
PatDS28 <johnpatrickdesalit@gmail.com>
2024-07-22 23:37:15 +0800

    script for adding books done, showing books in a div done

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function Book(author, title, pageNumber, haveRead) {
   this.pageNumber = pageNumber;
   this.haveRead = haveRead;
 }
-book1 = Book("Robert Greene", "40 Laws of Power", 452, "no")
-book2 = Book("Brianna West", "The Mountain is You", 241, "no")
+book1 = new Book("Robert Greene", "40 Laws of Power", 452, "no")
+book2 = new Book("Brianna West", "The Mountain is You", 241, "no")
 
 myLibrary.push(book1);
 myLibrary.push(book2);
@@ -18,3 +18,30 @@ function addBookToLibrary(author, title, pageNumber, haveRead) {
   book = new Book(author, title, pageNumber, haveRead);
   myLibrary.push(book);
 }
+
+function showBooks(author, title, pageNumber, haveRead){
+  const container = document.querySelector(".books-list-container")
+  const newDiv = document.createElement("div");
+  newDiv.className = "card";
+  const h2_title = document.createElement("h2")
+  h2_title.textContent = "Title: " + title;
+  const h4_author = document.createElement("h4")
+  h4_author.textContent = "Author: " + author;
+  const h4_pageNumber = document.createElement("h4")
+  h4_pageNumber.textContent = "Number of Page: " + pageNumber;
+  const h4_haveRead = document.createElement("h4")
+  h4_haveRead.textContent = "Have read: " + haveRead;
+  newDiv.appendChild(h2_title);
+  newDiv.appendChild(h4_author);
+  newDiv.appendChild(h4_pageNumber);
+  newDiv.appendChild(h4_haveRead);
+
+  container.appendChild(newDiv);
+}
+showBooks(myLibrary[0].author, myLibrary[0].author, myLibrary[0].author, myLibrary[0].author)
+
+// const container = document.querySelector(".books-list-container");
+// const ex = "ioqnodfipasnmopda"
+// container.innerHTML = myLibrary
+console.log(myLibrary)
+console.log(myLibrary[0].author)
